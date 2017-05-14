524f888192d8e5c5dcde995bb42778518e7e8677
philschonholzer <philip.schoenholzer@apptiva.ch>
2017-05-14 19:59:54 +0200

    Working bot
    - Fixed process.env problem
    - Get body data
    - Respond with correct format
    - Send status 200 in any case

--- handler.js
+++ handler.js
@@ -1,15 +1,26 @@
 const https = require('https'),
       qs = require('querystring'),
-      ACCESS_TOKEN = "[SLACK ACCESS TOKEN]";
+      VERIFICATION_TOKEN = process.env.VERIFICATION_TOKEN,
+      ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 
 // Verify Url - https://api.slack.com/events/url_verification
 function verify(data, callback) {
-    if (data.token === process.env.VERIFICATION_TOKEN) callback(null, data.challenge);
+    if (data.token === VERIFICATION_TOKEN) {
+        console.log('Verified - respond with challenge');
+        const response = {
+		    statusCode: 200,
+            headers : {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: data.challenge
+        };
+        callback(null, response);
+    }
     else callback("verification failed");   
 }
 
 // Post message to Slack - https://api.slack.com/methods/chat.postMessage
-function process(event, callback) {
+function postMessage(event, callback) {
     // test the message for a match and not a bot
     if (!event.bot_id && /(aws|lambda)/ig.test(event.text)) {
         var text = `<@${event.user}> isn't AWS Lambda awesome?`;
@@ -23,14 +34,19 @@ function process(event, callback) {
         https.get(`https://slack.com/api/chat.postMessage?${query}`);
     }
 
-    callback(null);
+    // Your app should respond to the event request with an HTTP 2xx within three seconds. 
+    // If it does not, we'll consider the event delivery attempt failed. 
+    // After a failure, we'll retry three times, backing off exponentially.
+    callback(null, {statusCode: 200});
 }
 
 // Lambda handler
-exports.event = (data, context, callback) => {
+exports.event = ({body}, context, callback) => {
+    console.log('Event:' + JSON.stringify(body));
+    let data = JSON.parse(body);
     switch (data.type) {
         case "url_verification": verify(data, callback); break;
-        case "event_callback": process(data.event, callback); break;
+        case "event_callback": postMessage(data.event, callback); break;
         default: callback(null);
     }
 };
